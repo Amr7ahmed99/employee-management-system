eb0372f2808e88469efe46e5182959669385a133
Amr7ahmed99 <amr.omar@maxab.io>
2023-05-27 21:44:54 +0300

    feat: Adding employee service layer, integrate with backend to fetch employees data.

--- Angular-frontend/src/app/employee-list/employee-list.component.ts
+++ Angular-frontend/src/app/employee-list/employee-list.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from '../employee';
+import { EmployeeService } from '../employee.service';
 
 @Component({
   selector: 'app-employee-list',
   templateUrl: './employee-list.component.html',
   styleUrls: ['./employee-list.component.css']
 })
-export class EmployeeListComponent {
+export class EmployeeListComponent implements OnInit{
+  
+  employees: Employee[];
+  ;
+  constructor( private employeeService: EmployeeService){}
 
+  ngOnInit(): void {
+    this.getEmployeesList();
+  }
+
+  private getEmployeesList(): void{
+    this.employeeService.getEmployeesList().subscribe(res=>{
+      this.employees= res;
+    });
+  }
 }
